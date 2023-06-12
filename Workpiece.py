d0ce724b03be8697ba81cb63d1c83c3e80337e5d
Escape9002 <antonio.rehwinkel@schiller-og.de>
2023-06-12 14:42:04 +0200

    catch up to main; takes:  0.00011384100071154535; no optimizations

--- Workpiece.py
+++ Workpiece.py
@@ -9,17 +9,18 @@ class Workpiece:
         for i in range(0, length):
             self.array.append(self.radius)
     
+    # sets the correct dimensions of the plot and plots the workpiece
     def visualize(self):
         plt.plot(self.array)
         plt.xlim(0, self.length)
         plt.ylim(0, self.radius * 2)
 
-# point is the index of the slice, depth the y-coordinate of the cut
+    # point is the index of the slice, depth the y-coordinate of the cut
     def cut(self, point: int, depth: float):
         self.array[point] = depth
 
-# cut to the same depth at multiple points
-# points: (from: inclusive, to: exclusive)
+    # cut to the same depth at multiple points
+    # points: (from: inclusive, to: exclusive)
     def cut_multiple(self, points: list[int, int], depth: float):
         for point in range(points[0], points[1] - 1):
             self.cut(point, depth)
--- turning-mill-v1.py
+++ turning-mill-v1.py
@@ -5,11 +5,21 @@ from Vector import Vector2
 from Workpiece import Workpiece
 from Tool import Tool
 
+import timeit
+import time
+
+# intialize workpiece with length 50 and radius 5
 wp = Workpiece(50, 5)
-wp.cut_multiple((2, 4), 3)
 
-tl = Tool(Vector2(0, 9), [1, 3, 6, 3, 1])
+# intialize the tool at the given position and set the shape
+tl = Tool(Vector2(15, 6), [1, 4, 6, 4, 1, 0, 0, 0, 1, 4, 6, 4, 1], wp.length)
 
+start_time = timeit.default_timer()
+# cut out the parts of the workpiece that are in contact with the tool
+tl.cut(wp)
+print(f'Takes running time: {timeit.default_timer() - start_time}')
+
+# plot the workpiece and the tool and show the plot
 tl.visualize()
 wp.visualize()
 plt.show()
