895c1a401c7017a068f3d616a39a9ce602736a5b
Escape9002 <antonio.rehwinkel@schiller-og.de>
2023-06-14 01:00:50 +0200

    added widht height pos, create file func, read func

--- SVGFormater.py
+++ SVGFormater.py
@@ -6,8 +6,18 @@ from Vector import Vector2
 import numpy as np
 
 # expected format:
-# <svg> <path [string]=[csv formatted string]/> </svg>
-# example: <svg> <path "heights"="0,6,1,2,4,3,2,1,9"/> </svg>
+# <svg xmlns="http://www.w3.org/2000/svg" 
+#   width=[string] 
+#   height=[string] 
+#   pos=[csv string]>
+#   <path [string]=[csv formatted string]/> </svg>
+
+# example: 
+# <svg xmlns="http://www.w3.org/2000/svg" 
+#   width="24" 
+#   height="24" 
+#   pos="0, 24">
+#   <path "heights"="0,6,1,2,4,3,2,1,9"/> </svg>
 
 class SVGFormater:
 
@@ -25,16 +35,70 @@ class SVGFormater:
         data = root[0].attrib[key]
         return data
 
-    def get_form_val(self, url: str, key:str):
+    # read file contents of our own standard format (specs see above)
+    # output style:
+    # np.array(
+    #   Vector2(widht, height), 
+    #   Vector2(pos.x,pos.y),
+    #   Vector2(data.x, data.y),
+    #   ...
+    #   Vector2(data.x, data.y),
+    #   )
+    def get_form_val(self, url: str):
         tree = ET.parse(url)
         root = tree.getroot()
-        data = root[0].attrib[key]
-        vektors = np.array(data.split(","))
-    
-        own_data = list()
-        for id, element in enumerate(vektors):
-            own_data.append(Vector2(id, element))
-        return np.array(own_data)
 
-# SVGFormater = SVGFormater()
-# data = SVGFormater.get_form_val(url='smiley.svg', key='d')
+        width = root[0].attrib["width"]
+        height = root[0].attrib["height"]
+        pos = root[0].attrib["pos"]
+        data = root[0].attrib["data"]
+
+        own_data = list()
+        own_data.append(Vector2(width,height))
+        
+        x,y = np.array(pos.split(","))
+        own_data.append(Vector2(x, y))
+
+        vectors = np.array(data.split(","))
+        for id, element in enumerate(vectors):
+            own_data.append(Vector2(id, element))
+        
+        return np.array(own_data)
+    
+    # specs:
+    # <svg xmlns="http://www.w3.org/2000/svg" 
+    #   width=[string] 
+    #   height=[string] 
+    #  pos=[csv string]>
+    #   <path [string]=[csv formatted string]/> </svg>
+
+    # expected input: 
+    # np.array(
+    #   Vector2(widht, height), 
+    #   Vector2(pos.x,pos.y),
+    #   Vector2(data.x, data.y),
+    #   ...
+    #   Vector2(data.x, data.y),
+    #   )
+    def create_form_file(self, url:str, data:np.array):
+        width = data[0].x 
+        height = data[0].y
+        pos = f'{data[1].x}, {data[1].y}'
+        data_string = ','.join([ str(num) for num in data])
+
+        data = f'<svg xmlns="http://www.w3.org/2000/svg" width="{width}" height="{height}" pos="{pos}"><path data="{data_string}"/> </svg>'
+        with open(url, "w") as file:
+            file.write(data)
+
+    def create_file(self, url: str, key:str, data: np.array):
+        data_string = ','.join([ str(num) for num in data])
+
+        data = f'<svg> <path {key}="{data_string}"/> </svg>'
+        with open(url, "w") as file:
+            file.write(data)
+
+SVGFormater = SVGFormater()
+data = SVGFormater.get_form_val(url='smiley.svg')
+
+SVGFormater.create_file("test.svg", "heights", np.array([5,5,5,55]))
+SVGFormater.create_form_file("form_test.svg", np.array((Vector2(5,5), Vector2(10,10), Vector2(10,10),Vector2(10,10),Vector2(10,10))))
