8d2a2c736495ddf2951fe27b3b9987c723540b8c
Escape9002 <antonio.rehwinkel@schiller-og.de>
2023-06-13 20:26:27 +0200

    visualize donuts and half donuts

--- Donut.py
+++ Donut.py
@@ -1,53 +1,65 @@
-import math
-
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
 import numpy as np
-# donut variables
-#   R as big radius
-#   r as tiny radius
-#   u as hight of circle (circle in y)
-#   v as x coord of big circle ( cirlce in x)
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# To compute all positions you need to iterate over u and v for (idk) 5 (?)
-# 
 
-#   x = (R + r * math.cos(u))* cos(v)
-#   y = (R + r * math.cos(u))* cos(v)
-#   z = r * sin(u)
+def elmos_donut(R: int, r: int, scale: int):
+    #   R as big radius (theta)
+    #   r as tiny radius    (phi)
+    #   theta as hight of circle (circle in y)  
+    #   phi as x coord of big circle ( cirlce in x)   
 
-def elmos_donut(R: int, r: int, scale: int, debug = False):
-    x = list()
-    y = list()
-    z = list()
+    # To compute all positions you need to iterate over theta and phi for 2*pi
 
-    for u in range(scale):
-        if (u % 100 == 0 and debug):
-            print(f'Running: {  ((u +1)/scale) * 100:.2f} % ')
-        for v in range(scale):
-            x.append((R + r * math.cos(u))* math.cos(v))
-            y.append((R + r * math.cos(u))* math.cos(v))
-            z.append(r * math.sin(u))
-    if(debug):
-        print("Done!")
+    #   x = (R + r * math.cos(theta))* cos(phi)
+    #   y = (R + r * math.cos(theta))* cos(phi)
+    #   z = r * sin(theta)
+
+    # create 1d array of values from 0 to 2*pi, with 2*pi / scale as resolution
+    # for both, theta and phi
+    theta = np.linspace(0, 2.*np.pi, scale)
+    phi = np.linspace(0, 2.*np.pi, scale)
+    theta, phi = np.meshgrid(theta, phi)
+    x = (R + r*np.cos(theta)) * np.cos(phi)
+    y = (R + r*np.cos(theta)) * np.sin(phi)
+    z = r * np.sin(theta)
+
     return (x,y,z)
 
-X,Y,Z = elmos_donut(2,1,2500, debug=True)
+def elmos_half_donut(R: int, r: int, scale: int, down = True):
+    # for 0 --> pi the upper half is modeled
+    # for pi --> 2*pi the lower half is modeled
+    # scale = scale / 2 because there would be double the datapoints 
+    # for this model compared to the normal (complete) model otherwise
+    
+    scale = int(scale / 2)
+    if(down):
+        theta = np.linspace(np.pi, 2.*np.pi, scale)
+    else:
+        theta = np.linspace(0, np.pi, scale)
+    
+    phi = np.linspace(0, 2.*np.pi, scale)
+
+    theta, phi = np.meshgrid(theta, phi)
+    X = (R + r*np.cos(theta)) * np.cos(phi)
+    Y = (R + r*np.cos(theta)) * np.sin(phi)
+    Z = r * np.sin(theta) 
+    return (X,Y,Z)
+
+##########################################
+scale = 100
+R, r = 5,2
+##########################################
+
+X1,Y1,Z1 = elmos_half_donut(R,r,scale, down=False)
+X2,Y2,Z2 = elmos_half_donut(R,r,scale, down=True)
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+ax1 = fig.add_subplot(121, projection='3d')
+ax1.set_zlim(-3,3)
 
-# Data for a three-dimensional line
-zline = np.array(Z)
-xline = np.array(X)
-yline = np.array(Y)
-# ax.plot3D(xline, yline, zline, 'gray')
+ax1.scatter(X1, Y1, Z1, color ="blue")
+ax1.scatter(X2, Y2, Z2, color = "red")
 
-# Data for three-dimensional scattered points
-# zdata = 15 * np.random.random(100)
-# xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-# ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-ax.scatter3D(xline, yline, zline, c=zline, cmap='Greens')
-
+ax1.view_init(36, 26)
 plt.show()
